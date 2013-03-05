e9e95ead1ebf14f06fa4cec8701324011e2f3aec
Chris Köbke <c.koebke@hotmail.de>
2013-03-05 18:12:07 +0100

    Moved the save methods inside the spritefont generator to the spritefont serializer.

--- Xemio.GameLibrary/Rendering/Fonts/Utility/SpriteFontGenerator.cs
+++ Xemio.GameLibrary/Rendering/Fonts/Utility/SpriteFontGenerator.cs
@@ -113,33 +113,6 @@ namespace Xemio.GameLibrary.Rendering.Fonts.Utility
 
             return spriteFont;
         }
-        /// <summary>
-        /// Saves the specified font.
-        /// </summary>
-        /// <param name="font">The font.</param>
-        /// <param name="fileName">Name of the file.</param>
-        public static void Save(SpriteFont font, string fileName)
-        {
-            using (FileStream stream = new FileStream(fileName, FileMode.Create))
-            {
-                SpriteFontGenerator.Save(font, stream);
-            }
-        }
-        /// <summary>
-        /// Saves the specified font.
-        /// </summary>
-        /// <param name="font">The font.</param>
-        /// <param name="stream">The stream.</param>
-        public static void Save(SpriteFont font, Stream stream)
-        {
-            using (BinaryWriter writer = new BinaryWriter(stream))
-            {
-                writer.Write(font.Kerning);
-                writer.Write(font.Spacing);
-
-                font.FontCache.Serialize(stream);
-            }
-        }
         #endregion
     }
 }
