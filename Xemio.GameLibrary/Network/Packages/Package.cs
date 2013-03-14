6c2a5e6a8036c2b3270aa7eb3e0061ed072fe2a9
Chris Köbke <c.koebke@hotmail.de>
2013-03-14 22:51:38 +0100

    Added package classes.

---
+++ Xemio.GameLibrary/Network/Packages/Package.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Xemio.GameLibrary.Common;
+using Xemio.GameLibrary.Common.Link;
+using Xemio.GameLibrary.Common.Extensions;
+
+namespace Xemio.GameLibrary.Network.Packages
+{
+    public abstract class Package : ILinkable
+    {
+        #region ILinkable<int> Member
+        /// <summary>
+        /// Gets the identifier.
+        /// </summary>
+        [ExcludeSync]
+        public int Identifier
+        {
+            get { return this.GetType().Name.GetHashCode(); }
+        }
+        #endregion
+    }
+}
---
+++ Xemio.GameLibrary/Network/Packages/PackageHandler.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Xemio.GameLibrary.Components;
+using Xemio.GameLibrary.Network.Events;
+using Xemio.GameLibrary.Events;
+
+namespace Xemio.GameLibrary.Network.Packages
+{
+    public class PackageHandler : IConstructable
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PackageHandler"/> class.
+        /// </summary>
+        public PackageHandler()
+        {
+            this._subscribers = new Dictionary<Type, ActionCollection<Package>>();
+        }
+        #endregion
+
+        #region Fields
+        private readonly Dictionary<Type, ActionCollection<Package>> _subscribers;
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Handles the specified event.
+        /// </summary>
+        /// <param name="packageEvent">The event.</param>
+        protected void HandleEvent(ReceivedPackageEvent packageEvent)
+        {
+            List<Type> types = new List<Type>();
+
+            Type packageType = packageEvent.Package.GetType();
+            Type currentType = packageType;
+
+            while (currentType.BaseType != null)
+            {
+                types.Add(currentType);
+                currentType = currentType.BaseType;
+            }
+
+            types.Add(currentType);
+
+            foreach (Type type in types)
+            {
+                if (this._subscribers.ContainsKey(type))
+                {
+                    foreach (Action<Package> action in this._subscribers[type])
+                    {
+                        action(packageEvent.Package);
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Subscribes the specified type.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="action">The action.</param>
+        public void Subscribe(Type type, Action<Package> action)
+        {
+            if (!this._subscribers.ContainsKey(type))
+            {
+                this._subscribers.Add(type, new ActionCollection<Package>());
+            }
+
+            this._subscribers[type].Add(action);
+        }
+        /// <summary>
+        /// Subscribes the specified action to arriving packages of the specified type.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="action">The action.</param>
+        public void Subscribe<T>(Action<T> action) where T : Package
+        {
+            this.Subscribe(typeof(T), package => action(package as T));
+        }
+        #endregion
+
+        #region IConstructable Member
+        /// <summary>
+        /// Constructs this instance.
+        /// </summary>
+        public void Construct()
+        {
+            EventManager eventManager = XGL.GetComponent<EventManager>();
+            eventManager.Subscribe<ReceivedPackageEvent>(this.HandleEvent);
+        }
+        #endregion
+    }
+}
