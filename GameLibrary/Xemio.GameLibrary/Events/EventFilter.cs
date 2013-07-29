ff32510cd1611c49f6c80538fbaa26310cbb8943
haefele <haefele@xemio.net>
2013-07-29 17:47:05 +0200

    Implemented an EventFilter, for filtering events.
    Also removed unneeded references from the XGL project.

---
+++ GameLibrary/Xemio.GameLibrary/Events/EventFilter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xemio.GameLibrary.Events
+{
+    public class EventFilter<T> where T : class, IEvent
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the action that will be executed.
+        /// </summary>
+        public Action<T> ExecuteAction { get; private set; }
+        /// <summary>
+        /// Gets the filters.
+        /// </summary>
+        public IList<Func<T, bool>> Filters { get; private set; }
+        #endregion Properties
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventFilter{T}"/> class.
+        /// </summary>
+        /// <param name="executeAction">The execute action.</param>
+        private EventFilter(Action<T> executeAction)
+        {
+            this.Filters = new List<Func<T, bool>>();
+            this.ExecuteAction = executeAction;
+        }
+        #endregion Constructors
+
+        #region Static Methods
+        /// <summary>
+        /// Creates a new EventFilter executing the given method.
+        /// </summary>
+        /// <param name="executeAction">The execute action.</param>
+        public static EventFilter<T> Execute(Action<T> executeAction)
+        {
+            return new EventFilter<T>(executeAction);
+        }
+        #endregion Static Methods
+
+        #region Methods
+        /// <summary>
+        /// Applies a filter function to this EventFilter.
+        /// The ex
+        /// </summary>
+        /// <param name="filter">The filter.</param>
+        public EventFilter<T> When(Func<T, bool> filter)
+        {
+            this.Filters.Add(filter);
+
+            return this;
+        }
+        /// <summary>
+        /// Creates the configured filter.
+        /// </summary>
+        public Action<T> Create()
+        {
+            return parameter =>
+                       {
+                           if (this.Filters.All(filter => filter(parameter)))
+                           {
+                               this.ExecuteAction(parameter);
+                           }
+                       };
+        }
+        #endregion Methods
+    }
+}
---
+++ Testing/Xemio.Testing.ReactiveExtensions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using Xemio.GameLibrary.Events;
+using Xemio.GameLibrary.Events.Logging;
+
+namespace Xemio.Testing.ReactiveExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventManager eventManager = new EventManager();
+
+            eventManager.Subscribe(EventFilter<LoggingEvent>
+                                       .Execute(Method)
+                                       .When(f => f.Level > LoggingLevel.Information)
+                                       .Create());
+            
+            eventManager.Publish(new LoggingEvent(LoggingLevel.Exception, "Hallo Welt"));
+
+            Console.ReadLine();
+        }
+
+        private static void Method(IEvent loggingEvent)
+        {
+            Console.WriteLine("Hallo Welt");
+        }
+    }
+}
