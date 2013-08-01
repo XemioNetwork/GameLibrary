deb8d0e09dfa4eaeddc8bc91a837d441f7e484e5
haefele <haefele@xemio.net>
2013-08-01 22:06:26 +0200

    Implemented a simple Profiler class.

---
+++ GameLibrary/Xemio.GameLibrary/Common/Profiler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Xemio.GameLibrary.Common
+{
+    public static class Profiler
+    {
+        /// <summary>
+        /// Executes the given action and logs the execution time.
+        /// </summary>
+        /// <param name="title">The name logged with the execution time.</param>
+        /// <param name="action">The action to execute.</param>
+        public static void Execute(string title, Action action)
+        {
+            var watch = Stopwatch.StartNew();
+
+            action();
+
+            watch.Stop();
+
+            Console.WriteLine("{0}: {1} ms", title, watch.Elapsed.TotalMilliseconds);
+        }
+    }
+}
