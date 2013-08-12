131c657b6ad06b0e853150880eea705198ebb437
haefele <haefele@xemio.net>
2013-08-12 03:10:35 +0200

    Removed an "error" in the GlobalExceptionHandler.
    The event handler is getting called twice.

--- GameLibrary/Xemio.GameLibrary/Common/GlobalExceptionHandler.cs
+++ GameLibrary/Xemio.GameLibrary/Common/GlobalExceptionHandler.cs
@@ -10,6 +10,14 @@ namespace Xemio.GameLibrary.Common
 {
     public class GlobalExceptionHandler : IConstructable
     {
+        #region Fields
+        /// <summary>
+        /// The last exception that was thrown.
+        /// Needed because for some reason the UnhandledException event will be called twice.
+        /// </summary>
+        private Exception _lastException;
+        #endregion Fields
+
         #region IConstructable Member
         /// <summary>
         /// Constructs this instance.
@@ -30,8 +38,13 @@ namespace Xemio.GameLibrary.Common
         {
             Exception exception = e.ExceptionObject as Exception;
 
-            EventManager eventManager = new EventManager();
-            eventManager.Publish(new ExceptionEvent(exception));
+            if (exception != this._lastException)
+            { 
+                EventManager eventManager = XGL.Components.Get<EventManager>();
+                eventManager.Publish(new ExceptionEvent(exception));
+
+                this._lastException = exception;
+            }
         }
         #endregion Private Methods
     }
