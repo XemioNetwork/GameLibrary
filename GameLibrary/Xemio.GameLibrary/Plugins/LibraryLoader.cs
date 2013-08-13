874e2e0cf0cb992386e3b0562ba56188cb50ff23
haefele <haefele@xemio.net>
2013-08-13 15:27:52 +0200

    Made the LibraryLoader more robust.

--- GameLibrary/Xemio.GameLibrary/Plugins/LibraryLoader.cs
+++ GameLibrary/Xemio.GameLibrary/Plugins/LibraryLoader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -17,12 +18,7 @@ namespace Xemio.GameLibrary.Plugins
         /// <param name="context">The context.</param>
         public void LoadLibraries(IAssemblyContext context)
         {
-            var initializers = from assembly in context.Assemblies
-                               from type in assembly.GetTypes()
-                               where
-                                   !type.IsAbstract && !type.IsInterface && !type.IsGenericType &&
-                                   typeof(ILibraryInitializer).IsAssignableFrom(type)
-                               select Activator.CreateInstance(type) as ILibraryInitializer;
+            var initializers = this.GetInitializer(context);
 
             foreach (var initializer in initializers)
             {
@@ -30,6 +26,68 @@ namespace Xemio.GameLibrary.Plugins
             }
         }
         #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Gets the initializers from the specified assembly context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        private IEnumerable<ILibraryInitializer> GetInitializer(IAssemblyContext context)
+        {
+            IList<Assembly> failedAssemblies = new List<Assembly>();
+
+            foreach (Assembly assembly in context.Assemblies)
+            {
+                IEnumerable<ILibraryInitializer> initializers;
+                if (!this.TryGetInitializerFromAssembly(assembly, out initializers))
+                {
+                    failedAssemblies.Add(assembly);
+                }
+
+                foreach (ILibraryInitializer initializer in initializers)
+                {
+                    yield return initializer;
+                }
+            }
+
+            //Retry all failed assemblies
+            foreach (Assembly assembly in failedAssemblies)
+            {
+                IEnumerable<ILibraryInitializer> initializers;
+                //Ignore if they fail now
+                this.TryGetInitializerFromAssembly(assembly, out initializers);
+
+                foreach (ILibraryInitializer initializer in initializers)
+                {
+                    yield return initializer;
+                }
+            }
+        }
+        /// <summary>
+        /// Tries to get the initializer from the specified assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <param name="result">The result.</param>
+        private bool TryGetInitializerFromAssembly(Assembly assembly, out IEnumerable<ILibraryInitializer> result)
+        {
+            try
+            {
+                result = from type in assembly.GetTypes()
+                         where
+                             !type.IsAbstract && !type.IsInterface && !type.IsGenericType &&
+                             typeof(ILibraryInitializer).IsAssignableFrom(type)
+                         select Activator.CreateInstance(type) as ILibraryInitializer;
+
+                return true;
+            }
+            catch (ReflectionTypeLoadException)
+            {
+                result = new List<ILibraryInitializer>();
+
+                return false;
+            }
+        }
+        #endregion Private Methods
 
         #region Implementation of IConstructable
         /// <summary>
