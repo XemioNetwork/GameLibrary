e02ce19269678ab2b67fe06758e2efac232fba97
ChrisKoebke <c.koebke@hotmail.de>
2014-02-11 21:11:04 +0100

    Cleared input testing code.

--- Testing/Xemio.Testing.Input/frmInput.Designer.cs
+++ Testing/Xemio.Testing.Input/frmInput.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 642);
+            this.Name = "frmInput";
             this.Text = "frmInput";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
