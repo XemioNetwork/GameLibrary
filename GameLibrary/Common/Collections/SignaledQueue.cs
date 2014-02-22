36bd8e661682e3fa0bd062381c62fe7cb05786f4
ChrisKoebke <c.koebke@hotmail.de>
2014-02-22 01:27:02 +0100

    Moved SignaledQueue to the Common.Collections namespace.

--- GameLibrary/Common/Collections/SignaledQueue.cs
+++ GameLibrary/Common/Collections/SignaledQueue.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading;
 
-namespace Xemio.GameLibrary.Network.Protocols.Local
+namespace Xemio.GameLibrary.Common.Collections
 {
     public class SignaledQueue<T>
     {
--- GameLibrary/Network/Protocols/Local/LocalBridge.cs
+++ GameLibrary/Network/Protocols/Local/LocalBridge.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xemio.GameLibrary.Common.Collections;
 using Xemio.GameLibrary.Network.Packages;
 
 namespace Xemio.GameLibrary.Network.Protocols.Local
